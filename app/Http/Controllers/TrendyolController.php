8fb9704279072be23fe4db915edb96f475031c25
Electronic12 <sowgatfilm@gmail.com>
2023-10-24 09:48:33 +0500

    trendurl

--- app/Http/Controllers/TrendyolController.php
+++ app/Http/Controllers/TrendyolController.php
@@ -9,6 +9,107 @@ use App\Models\Product;
 
 class TrendyolController extends Controller
 {
+    public function trendyolurl()
+    {
+        DB::table('productsurl')->delete();
+        $client = new Client();
+        $urls = [
+            "https://www.trendyol.com/bershka-kadin-ceket-x-b486-g1-c1030?pi=1",
+            "https://www.trendyol.com/bershka-kadin-ceket-x-b486-g1-c1030?pi=2",
+            "https://www.trendyol.com/bershka-kadin-ceket-x-b486-g1-c1030?pi=3",
+            "https://www.trendyol.com/bershka-kadin-ceket-x-b486-g1-c1030?pi=4",
+            "https://www.trendyol.com/pull-bear-kadin-ceket-x-b592-g1-c1030?pi=1",
+            "https://www.trendyol.com/pull-bear-kadin-ceket-x-b592-g1-c1030?pi=2",
+            "https://www.trendyol.com/pull-bear-kadin-ceket-x-b592-g1-c1030?pi=3",
+            "https://www.trendyol.com/pull-bear-kadin-ceket-x-b592-g1-c1030?pi=4",
+            "https://www.trendyol.com/stradivarius-kadin-ceket-x-b147-g1-c1030?pi=1",
+            "https://www.trendyol.com/stradivarius-kadin-ceket-x-b147-g1-c1030?pi=2",
+            "https://www.trendyol.com/stradivarius-kadin-ceket-x-b147-g1-c1030?pi=3",
+            "https://www.trendyol.com/stradivarius-kadin-ceket-x-b147-g1-c1030?pi=4",
+            "https://www.trendyol.com/stradivarius-kadin-ceket-x-b147-g1-c1030?pi=5",
+            "https://www.trendyol.com/stradivarius-kadin-ceket-x-b147-g1-c1030?pi=6",
+            "https://www.trendyol.com/mavi-kadin-ceket-x-b43-g1-c1030?pi=1",
+            "https://www.trendyol.com/mavi-kadin-ceket-x-b43-g1-c1030?pi=2",
+            "https://www.trendyol.com/mavi-kadin-ceket-x-b43-g1-c1030?pi=3",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=1",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=2",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=3",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=4",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=5",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=6",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=7",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=8",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=9",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=10",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=11",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=12",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=13",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=14",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=15",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=16",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=17",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=18",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=19",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=20",
+            "https://www.trendyol.com/koton-kadin-ceket-x-b38-g1-c1030?pi=21",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=1",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=2",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=3",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=4",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=5",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=6",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=7",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=8",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=9",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=10",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=11",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=12",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=13",
+            "https://www.trendyol.com/mango-kadin-ceket-x-b41-g1-c1030?pi=14",
+            "https://www.trendyol.com/defacto-kadin-ceket-x-b37-g1-c1030?pi=1",
+            "https://www.trendyol.com/defacto-kadin-ceket-x-b37-g1-c1030?pi=2",
+            "https://www.trendyol.com/defacto-kadin-ceket-x-b37-g1-c1030?pi=3",
+            "https://www.trendyol.com/defacto-kadin-ceket-x-b37-g1-c1030?pi=4",
+            "https://www.trendyol.com/defacto-kadin-ceket-x-b37-g1-c1030?pi=5",
+            "https://www.trendyol.com/defacto-kadin-ceket-x-b37-g1-c1030?pi=6",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=1",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=2",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=3",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=4",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=5",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=6",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=7",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=8",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=9",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=10",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=11",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=12",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=13",
+            "https://www.trendyol.com/lc-waikiki-kadin-ceket-x-b859-g1-c1030?pi=14",
+            "https://www.trendyol.com/bianco-lucci-kadin-ceket-x-b108614-g1-c1030?pi=1",
+            "https://www.trendyol.com/bianco-lucci-kadin-ceket-x-b108614-g1-c1030?pi=2",
+            "https://www.trendyol.com/bianco-lucci-kadin-ceket-x-b108614-g1-c1030?pi=3",
+            "https://www.trendyol.com/bianco-lucci-kadin-ceket-x-b108614-g1-c1030?pi=4",
+            "https://www.trendyol.com/bianco-lucci-kadin-ceket-x-b108614-g1-c1030?pi=5",
+            "https://www.trendyol.com/bianco-lucci-kadin-ceket-x-b108614-g1-c1030?pi=6",
+            "https://www.trendyol.com/bianco-lucci-kadin-ceket-x-b108614-g1-c1030?pi=7",
+            "https://www.trendyol.com/grimelange-kadin-ceket-x-b110329-g1-c1030",
+            "https://www.trendyol.com/asics-kadin-ceket-x-b145479-g1-c1030",
+        ];
+
+        foreach ($urls as $url) {
+            $response = $client->request('GET', $url);
+            $productLinks = $response->filter('.p-card-chldrn-cntnr.card-border a')->links();
+
+            foreach ($productLinks as $link) {
+                $linkUrl = $link->getUri();
+                $brandspl = explode("/", $linkUrl);
+                $brand = $brandspl[3];
+                DB::insert("INSERT INTO productsurl (brands, links, created_at, updated_at) VALUES (?, ?, ?, ?)", [$brand, $linkUrl, now(), now()]);
+            }
+        }
+    }
+
     public function products(Request $request)
     {
         $batchSize = 200;
--- routes/web.php
+++ routes/web.php
@@ -1,84 +1,18 @@
 <?php
 
-use App\Http\Controllers\ProductsDB;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\BershkaController;
-use App\Http\Controllers\PullBearController;
-use App\Http\Controllers\KotonController;
-use App\Http\Controllers\AsicsController;
-use App\Http\Controllers\StradivariusController;
-use App\Http\Controllers\DefactoController;
-use App\Http\Controllers\BiancoLucciController;
-use App\Http\Controllers\MangoController;
-use App\Http\Controllers\GrimelangeController;
-use App\Http\Controllers\MaviController;
-use App\Http\Controllers\LCWaikikiController;
+use App\Http\Controllers\TrendyolController;
 use App\Http\Controllers\FacesController;
 
 Route::get('/allproducts', [ProductController::class, 'allproducts'])->name('import-view');
 Route::post('/import', [ProductController::class, 'import'])->name('import');
 Route::get('/export', [ProductController::class, 'export'])->name('export');
-Route::get('/url', [ProductController::class, 'url']);
+Route::get('/trendyolurl', [ProductController::class, 'trendyolurl']);
 Route::get('/', [ProductController::class, 'product_all']);
 Route::get('/delete', [ProductController::class, 'delete']);
 
-Route::get('/products', [ProductsDB::class, 'products']);
-Route::get('/dbasics', [AsicsController::class, 'dbasics']);
-Route::get('/getasics', [AsicsController::class, 'getasics']);
-
-Route::get('/allbershka', [BershkaController::class, 'allbershka']);
-Route::get('/dbbershka', [BershkaController::class, 'dbbershka']);
-Route::get('/getbershka', [BershkaController::class, 'getbershka']);
-
-Route::get('/allbiancolucci', [BiancoLucciController::class, 'allbiancolucci']);
-Route::get('/dbbiancolucci', [BiancoLucciController::class, 'dbbiancolucci']);
-Route::get('/dbbiancolucci1', [BiancoLucciController::class, 'dbbiancolucci1']);
-Route::get('/getbiancolucci', [BiancoLucciController::class, 'getbiancolucci']);
-
-Route::get('/alldefacto', [DefactoController::class, 'alldefacto']);
-Route::get('/dbdefacto', [DefactoController::class, 'dbdefacto']);
-Route::get('/dbdefacto1', [DefactoController::class, 'dbdefacto1']);
-Route::get('/getdefacto', [DefactoController::class, 'getdefacto']);
-
-Route::get('/allgrimelange', [GrimelangeController::class, 'allgrimelange']);
-Route::get('/dbgrimelange', [GrimelangeController::class, 'dbgrimelange']);
-Route::get('/getgrimelange', [GrimelangeController::class, 'getgrimelange']);
-
-Route::get('/allkoton', [KotonController::class, 'allkoton']);
-Route::get('/dbkoton1', [KotonController::class, 'dbkoton1']);
-Route::get('/dbkoton2', [KotonController::class, 'dbkoton2']);
-Route::get('/dbkoton3', [KotonController::class, 'dbkoton3']);
-Route::get('/dbkoton4', [KotonController::class, 'dbkoton4']);
-Route::get('/dbkoton5', [KotonController::class, 'dbkoton5']);
-Route::get('/getkoton', [KotonController::class, 'getkoton']);
-
-Route::get('/alllcwaikiki', [LCWaikikiController::class, 'alllcwaikiki']);
-Route::get('/dblcwaikiki', [LCWaikikiController::class, 'dblcwaikiki']);
-Route::get('/dblcwaikiki1', [LCWaikikiController::class, 'dblcwaikiki1']);
-Route::get('/dblcwaikiki2', [LCWaikikiController::class, 'dblcwaikiki2']);
-Route::get('/dblcwaikiki3', [LCWaikikiController::class, 'dblcwaikiki3']);
-Route::get('/getlcwaikiki', [LCWaikikiController::class, 'getlcwaikiki']);
-
-Route::get('/allmango', [MangoController::class, 'allmango']);
-Route::get('/dbmango', [MangoController::class, 'dbmango']);
-Route::get('/dbmango1', [MangoController::class, 'dbmango1']);
-Route::get('/getmango', [MangoController::class, 'getmango']);
-
-Route::get('/allmavi', [MaviController::class, 'allmavi']);
-Route::get('/dbmavi', [MaviController::class, 'dbmavi']);
-Route::get('/getmavi', [MaviController::class, 'getmavi']);
-
-Route::get('/allpullbear', [PullBearController::class, 'allpullbear']);
-Route::get('/dbpullbear', [PullBearController::class, 'dbpullbear']);
-Route::get('/getpullbear', [PullBearController::class, 'getpullbear']);
-
-Route::get('/allstradivarius', [StradivariusController::class, 'allstradivarius']);
-Route::get('/dbstradivarius', [StradivariusController::class, 'dbstradivarius']);
-Route::get('/dbstradivarius1', [StradivariusController::class, 'dbstradivarius1']);
-Route::get('/getstradivarius', [StradivariusController::class, 'getstradivarius']);
-
+Route::get('/products', [TrendyolController::class, 'products']);
 
 Route::get('/getfaces', [FacesController::class, 'getfaces']);
 Route::get('/getFacesProducts', [FacesController::class, 'getFacesProducts']);
